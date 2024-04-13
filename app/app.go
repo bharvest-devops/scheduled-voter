986366afe91178cf0e68c57959ccceb8f66eebc2
Choi Hocheol <show1693@gmail.com>
2024-04-13 16:50:14 +0900

    feat: Add infinite running feature

--- app/app.go
+++ app/app.go
@@ -23,18 +23,36 @@ func (app *BaseApp) Run(ctx context.Context) {
 	utils.Info(fmt.Sprintf("Start scheduled voter for %d minutes", app.cfg.Vote.Duration))
 
 	go app.SubProposal(appCtx)
-	for {
-		select {
-		case propId := <-app.chSubProposal:
-			// ProcVote will be thread safe
-			// because of sequence of vote tx
-			app.ProcVote(appCtx, propId)
-		case <-time.After(time.Duration(app.cfg.Vote.Duration) * time.Minute):
-			utils.Info("Program ended")
-			return
-		case err := <-app.chErr:
-			utils.Error(err)
-			return
+	app.StartVoteProposal(appCtx)
+}
+
+func (app *BaseApp) StartVoteProposal(ctx context.Context) {
+	if app.cfg.Vote.Duration == 0 {
+		for {
+			select {
+			case propId := <-app.chSubProposal:
+				// ProcVote will be thread safe
+				// because of sequence of vote tx
+				app.ProcVote(ctx, propId)
+			case err := <-app.chErr:
+				utils.Error(err)
+				return
+			}
+		}
+	} else {
+		for {
+			select {
+			case propId := <-app.chSubProposal:
+				// ProcVote will be thread safe
+				// because of sequence of vote tx
+				app.ProcVote(ctx, propId)
+			case <-time.After(time.Duration(app.cfg.Vote.Duration) * time.Minute):
+				utils.Info("Program ended")
+				return
+			case err := <-app.chErr:
+				utils.Error(err)
+				return
+			}
 		}
 	}
 }
